c17f9e5e4717acbbabdcfa70af14d6dcd7838d18
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-04-17 14:50:49 +0000

    Add Logo, Favicon icon and Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from '../styles/NavBar.module.css'
+import logo from '../assets/logo.png';
+
+
+const NavBar = () => {
+  return (
+    <div>
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand className= {styles.NavBarBrand}>
+          <img src={logo} alt="logo" height="65" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default NavBar
