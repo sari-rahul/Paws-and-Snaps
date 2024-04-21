a79222b20bcc302785d063c988cc1a15d7265f6b
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-04-21 15:24:43 +0000

    Add Avatar, Nav links and logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,73 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut= async()=>{
+    try{
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    }catch(err){
+      console.log(err)
+    }
+  };
+
+  const add = (
+  <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/post/create" >   
+    Add Article
+  </NavLink>)
+
+  const loggedInIcons = <>
+   {/*Blog drop down menu */}
+      <NavDropdown className ={styles.NavDropdown} title="Blogs" id="basic-nav-dropdown" >
+        <NavDropdown.Item href="#action/3.1">Dogs</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Cats</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Others</NavDropdown.Item>
+      </NavDropdown>
+   {/*Adoption link*/}
+      <NavLink
+        className={styles.NavLink}
+        to="/"     
+      >
+        Adoption
+      </NavLink>
+      {/*Wildlife link*/}
+      <NavDropdown className ={styles.NavDropdown} title="Wildlife" id="basic-nav-dropdown" >
+        <NavDropdown.Item href="#action/3.1">Animal Entcounters</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Environment</NavDropdown.Item>
+      </NavDropdown>
+      {/*Wellness link*/}
+      <NavLink
+        className={styles.NavLink}
+        to="/"     
+      >
+        Wellness
+      </NavLink>
+      {/*Logout link*/}
+      <NavLink
+        className={styles.NavLink}
+        to="/"
+        onClick={handleSignOut}
+      > Log out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text= "Profile" height={40}/>
+      </NavLink>
+  </>;
   const loggedOutIcons = (
     <>
       <NavLink
@@ -36,6 +95,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="65" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && add}
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
