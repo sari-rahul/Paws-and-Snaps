96e53d103d722ea0cfca646904bbbf64665bb54f
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-05-15 17:03:10 +0000

    Add 404 error page to assets

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Notfound from '../assets/not found.jpg';
+import ErrorPage from '../assets/404-page.png';
 import { Container } from 'react-bootstrap';
 import Asset from '../components/Assets.js';
 import appStyles from "../App.module.css"
 const NotFound = () => {
   return (
     <Container className={appStyles.AssetContainer}>
-        <Asset src={Notfound} message={'The Page Is Not Found'}/>
+        <Asset src={ErrorPage} message={'The Page Is Not Found'}/>
     </Container>
   )
 }
